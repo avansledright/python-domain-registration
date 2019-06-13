a813410a5b92f6ace44f898c0eec0dda9319900f
Aaron VanSledright <avansledright@gmail.com>
2019-06-13 16:30:54 -0400

    Added pretty print for JSON return

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import boto3
+import pprint
 
 domain = input("What domain do you want?: ")
 client = boto3.client('route53domains')
 
 response = client.check_domain_availability(
         DomainName=domain )
-print(response)
+pprint.pprint(response)
